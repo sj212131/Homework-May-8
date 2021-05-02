243cc23c8f2d4f87d1e8dabfd2f71ca9391dfc18
shangxian Piao <sj212131@hotmail.com>
2021-05-02 15:06:56 -0400

    updated typo

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PD,
+    process.env.DB_PASSWORD,
     {
       host: "localhost",
       dialect: "mysql",
